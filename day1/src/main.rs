1f17f25cef777abea3b7813fa6d55c811ab747c1
David Wilemski <david@wilemski.org>
2019-12-01 15:56:16 -0800

    Handle recursive fuel addition
    We have to have fuel for our fuel! This uses a stack based approach to
    handle solving for that.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,11 +8,33 @@ fn compute_fuel(mass: i32) -> i32 {
     (mass / 3) - 2
 }
 
+fn compute_additional_fuel(initial_fuel: i32) -> i32 {
+    let mut stack = vec![initial_fuel];
+
+    loop {
+        if let Some(prev) = stack.pop() {
+            stack.push(prev);
+            let result = compute_fuel(prev);
+            if result > 0 {
+                stack.push(result);
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    stack.iter().sum::<i32>()
+}
+
 fn main() -> Result<(), std::io::Error> {
     let stdin = std::io::stdin();
     let reader = BufReader::new(stdin);
 
-    println!("{}", reader.lines().map(|line| compute_fuel(line.unwrap().parse().unwrap())).sum::<i32>());
+    let fuel = reader.lines().map(|line| compute_additional_fuel(compute_fuel(line.unwrap().parse().unwrap()))).sum::<i32>();
+    println!("fuel: {}", fuel);
+
     Ok(())
 }
 
@@ -26,5 +48,13 @@ mod tests {
         assert_eq!(compute_fuel(14), 2);
         assert_eq!(compute_fuel(1969), 654);
         assert_eq!(compute_fuel(100756), 33583);
+    }
+
+    #[test]
+    fn it_handles_additional_fuel() {
+        assert_eq!(compute_additional_fuel(compute_fuel(12)), 2);
+        assert_eq!(compute_additional_fuel(compute_fuel(14)), 2);
+        assert_eq!(compute_additional_fuel(compute_fuel(1969)), 966);
+        assert_eq!(compute_additional_fuel(compute_fuel(100756)), 50346);
     }
 }
