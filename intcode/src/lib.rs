0ea745d1bf63ff665c3269f84c325d9560c06c65
David Wilemski <david@wilemski.org>
2019-12-04 22:12:23 -0800

    handle position and immediate loading modes

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -14,29 +14,30 @@ impl Machine {
 
     pub fn execute(self: &mut Self) {
         loop {
-            let instr = self.load(self.program_counter);
+            let (instr, mode_op1, mode_op2, _) = self.get_instr_and_modes();
             match instr {
                 99 => {
                     println!("HALT");
                     break;
                 }
                 1 => {
-                    let op1 = self.load(self.load(self.program_counter + 1));
-                    let op2 = self.load(self.load(self.program_counter + 2));
+                    let op1 = self.load_with_mode(self.program_counter + 1, mode_op1);
+                    let op2 = self.load_with_mode(self.program_counter + 2, mode_op2);
                     let out_reg = self.load(self.program_counter + 3);
                     println!("ADD {} {} into {}", op1, op2, out_reg);
 
                     self.store(out_reg, op1 + op2);
                 }
                 2 => {
-                    let op1 = self.load(self.load(self.program_counter + 1));
-                    let op2 = self.load(self.load(self.program_counter + 2));
+                    let op1 = self.load_with_mode(self.program_counter + 1, mode_op1);
+                    let op2 = self.load_with_mode(self.program_counter + 2, mode_op2);
                     let out_reg = self.load(self.program_counter + 3);
                     println!("MULT {} {} into {}", op1, op2, out_reg);
 
                     self.store(out_reg, op1 * op2);
                 }
                 3 => {
+                    //assert!(mode_op1 == 1);
                     let op1_addr = self.load(self.program_counter + 1);
                     println!("STORE_INPUT {} to {}", op1_addr, op1_addr);
 
@@ -65,6 +66,45 @@ impl Machine {
         }
     }
 
+    // ABCDE
+    //  1002
+
+    // DE - two-digit opcode,      02 == opcode 2
+    // C - mode of 1st parameter,  0 == position mode
+    // B - mode of 2nd parameter,  1 == immediate mode
+    // A - mode of 3rd parameter,  0 == position mode,
+       //                               omitted due to being a leading zero
+    // Parameters that an instruction writes to will never be in immediate mode.
+    // return opcode first and return modes as items 1, 2, 3 of tuple
+    fn get_instr_and_modes(self: &Self) -> (i32, i32, i32, i32) {
+
+        let instr = self.load(self.program_counter);
+        let digits_str = instr.to_string();
+        let digits = (0..digits_str.len()).map(|i| digits_str.get(i..i+1).unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        println!("{:?}", digits);
+
+        match digits_str.len() {
+            1 => {
+                (digits[0], 0, 0, 0)
+            }
+            2 => {
+                (digits[0] * 10 + digits[1], 0, 0, 0)
+            }
+            3 => {
+                (digits[0] * 10 + digits[1], digits[2], 0, 0)
+            }
+            4 => {
+                (digits[0] * 10 + digits[1], digits[2], digits[3], 0)
+            }
+            5 => {
+                unreachable!("theoretically possible but shouldn't happen because param 3 should never be in immediate mode: {:?}", digits);
+            }
+            _ => {
+                unreachable!("input integer too long: {:?}", digits);
+            }
+        }
+    }
+
     pub fn output(self: &Self) -> i32 {
         self.memory[0]
     }
@@ -81,6 +121,14 @@ impl Machine {
         let result = self.memory[addr as usize];
         println!("LOADING addr: {}, val: {}", addr, result);
         result
+    }
+
+    fn load_with_mode(self: &Self, addr: i32, mode: i32) -> i32 {
+        match mode {
+            0 => self.load(self.load(addr)),
+            1 => self.load(addr),
+            _ => unreachable!("invalid mode: {}", mode)
+        }
     }
 
     fn store(self: &mut Self, addr: i32, val: i32) {
